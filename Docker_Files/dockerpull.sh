080f474ca4d01241a7458a13443a6a0d76b7daa6
jgw004 <williamson_j@rocketmail.com>
2023-11-30 00:58:37 -0600

    further modification of docker pull files

--- Docker_Files/dockerpull.sh
+++ Docker_Files/dockerpull.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-## Purpose is to allow user input for '#docker pull' of an image.
+## Purpose is to allow users to pull a single docker image.
+###an improvement would be to increase 'count' and the index of a list containing \
+### available options to pull.
 
-# Pull Options
-$1=Nginx
-$2=helloworld
-$3=Wordpress
-$4=Postgresql
+
+echo ""
+echo " _"
 
 # Collect User Input
-echo Hello, which docker image would you like to pull today?
-echo 'Nginx'[1] or 'helloworld'[2] or 'Wordpress'[3] or 'Postgresql'[4]
+echo "  Which docker image would you like to pull today?"
+echo "  'Nginx'[1] or 'helloworld'[2] or 'Wordpress'[3] or 'Postgresql'[4]"
 read num_inp
 
-echo You have chosen $num_inp. Is this correct [y,n]?
+echo "  You have chosen #$num_inp.. Is this correct [y,n]?"
 read answer
 
 if [ $answer = 'y' ] ; then
     if [ $num_inp -eq 1 ] ; then
-        echo Pulling Nginx image...
+        echo "Pulling image image..."
         #docker pull nginx
     elif [ $num_inp -eq 2 ] ; then
-        echo Pulling helloworld image...
+        echo "Pulling helloworld image..."
         #docker pull helloworld
     elif [ $num_inp -eq 3 ] ; then
-        echo Pulling Wordpress image...
+        echo "Pulling Wordpress image..."
         #docker pull wordpress
     elif [ $num_inp -eq 4 ] ; then
-        echo Pulling Postgresql image...
+        echo "Pulling Postgresql image..."
         #docker pull postgres
     else
-        echo Invalid input, please try again.
+        echo "."
+        echo "Invalid input, please try again."
     fi
 else
-    echo Please enter a valid number next time..
-    echo ........................
+    echo "-"
     ./dockerpull.sh
 fi
+
+
