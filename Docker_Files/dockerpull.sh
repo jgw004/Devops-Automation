53f4040aad072c3c874e58303aa42244a214ae83
jgw004 <williamson_j@rocketmail.com>
2023-12-05 16:00:52 -0600

    modified dockerpull.sh and multicontainer.sh for tagging

--- Docker_Files/dockerpull.sh
+++ Docker_Files/dockerpull.sh
@@ -15,22 +15,54 @@ read num_inp
 echo "  You have chosen $num_inp.. Is this correct [y,n]?"
 read answer
 
+# Ask user if theyre happy with their option\
+#  and if they want to add a tag to their image
 if [ $answer = 'y' ] ; then
-    if [ $num_inp -eq 1 ] ; then
-        echo "Pulling Nginx image..."
-        docker pull nginx
-    elif [ $num_inp -eq 2 ] ; then
-        echo "Pulling helloworld image..."
-        docker pull helloworld
-    elif [ $num_inp -eq 3 ] ; then
-        echo "Pulling Wordpress image..."
-        docker pull wordpress
-    elif [ $num_inp -eq 4 ] ; then
-        echo "Pulling Postgresql image..."
-        docker pull postgres
+
+    echo "  Great! Would you like to tag this image [y,n]?"
+    read tag_option
+
+## Yes tag
+    if [ $tag_option = 'y' ] ; then
+	echo "  Okay, what would you like the tag to say?"
+	read tag_answer
+
+    	if [ $num_inp -eq 1 ] ; then
+    	    echo "Pulling Nginx image as $tag_answer..."
+    	    docker pull nginx:$tag_answer
+    	elif [ $num_inp -eq 2 ] ; then
+    	    echo "Pulling helloworld image as $tag_answer..."
+    	    docker pull helloworld:$tag_answer
+    	elif [ $num_inp -eq 3 ] ; then
+    	    echo "Pulling Wordpress image as $tag_answer..."
+    	    docker pull wordpress:$tag_answer
+    	elif [ $num_inp -eq 4 ] ; then
+    	    echo "Pulling Postgresql image as $tag_answer..."
+    	    docker pull postgres:$tag_answer
+    	else
+    	    echo "."
+    	    echo "Invalid input, please try again."
+	    ./dockerpull.sh
+    	fi
+## No tag
     else
-        echo "."
-        echo "Invalid input, please try again."
+        if [ $num_inp -eq 1 ] ; then
+            echo "Pulling Nginx image..."
+            docker pull nginx
+        elif [ $num_inp -eq 2 ] ; then
+            echo "Pulling helloworld image..."
+            docker pull helloworld
+        elif [ $num_inp -eq 3 ] ; then
+            echo "Pulling Wordpress image..."
+            docker pull wordpress
+        elif [ $num_inp -eq 4 ] ; then
+            echo "Pulling Postgresql image..."
+            docker pull postgres
+        else
+            echo "."
+            echo "Invalid input, please try again."
+	    ./dockerpull.sh
+        fi
     fi
 else
     echo "-"
